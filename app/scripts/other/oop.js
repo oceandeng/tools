4285080233de0f53440a6ffd452d9da67942949e
ocean_deng <ocean_deng@163.com>
2015-09-05 16:17:26 +0800

    update

--- app/scripts/other/oop.js
+++ app/scripts/other/oop.js
@@ -2,10 +2,20 @@
 * @Author: ocean
 * @Date:   2015-08-30 21:19:52
 * @Last Modified by:   ocean
-* @Last Modified time: 2015-08-30 21:32:58
+* @Last Modified time: 2015-09-05 16:16:57
 */
 
 'use strict';
+
+// 递归
+var factorial = (function fn(num){
+	if(num <= 1){
+		return 1;
+	}else{
+		return num * fn(num - 1);
+	}
+})();
+
 
 // 构造函数和原型组合
 function Person(name, age, job){
