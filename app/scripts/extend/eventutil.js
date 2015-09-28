d97059e84f607b7e4786c9c34e06cee4b46297a1
oceandeng <ocean_deng@163.com>
2015-09-28 09:50:20 +0800

    update

--- app/scripts/extend/eventutil.js
+++ app/scripts/extend/eventutil.js
@@ -2,7 +2,7 @@
 * @Author: ocean
 * @Date:   2015-06-28 19:13:05
 * @Last Modified by:   ocean
-* @Last Modified time: 2015-08-22 17:46:26
+* @Last Modified time: 2015-09-24 10:21:19
 */
 
 'use strict';
@@ -133,3 +133,27 @@ var EventUtil = {
 		event.initCustomEvent('myevent', true, false, 'Hello World!');
 		div.dispatchEvent(event);
 	}
+
+
+// 监测是touch事件
+function handleTouchEvent(event){
+	// 只跟踪一次
+	if(event.changedTouches.length == 1){
+		var output = document.getElementById("output");
+		switch(event.type){
+			case "touchstart":
+				output.innerHTML = "Touch started (" + event.touches[0].clientX + "," + event.touches[0].clientY + ")";
+				break;
+			case "touchend":
+				output.innerHTML += "<br>Touch ended (" + event.touches[0].clientX + "," + event.touches[0].clientY + ")";
+				break;
+			case "touchmove":
+				event.preventDefault();
+				output.innerHTML += "<br>Touch moved (" + event.changedTouches[0].clientX + "," + event.changedTouches[0].clientY + ")";
+				break;
+		}
+	}
+}
+// EventUtil.addHandler(document, "touchstart", handleTouchEvent);
+// EventUtil.addHandler(document, "touchend", handleTouchEvent);
+// EventUtil.addHandler(document, "touchmove", handleTouchEvent);
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -122,26 +122,3 @@ console.log(oTools.getAbsoluteURL('public'));
 // 		}
 // 	}
 // });
-
-// 监测是touch事件
-function handleTouchEvent(event){
-	// 只跟踪一次
-	if(event.touchs.length == 1){
-		var output = document.getElementById("output");
-		switch(event.type){
-			case "touchstart":
-				output.innerHTML = "Touch started (" + event.touches[0].clientX + "," + event.touches[0].clientY + ")";
-				break;
-			case "touchend":
-				output.innerHTML += "<br>Touch ended (" + event.touches[0].clientX + "," + event.touches[0].clientY + ")";
-				break;
-			case "touchmove":
-				event.preventDefault();
-				output.innerHTML += "<br>Touch moved (" + event.changedTouches[0].clientX + "," + event.changedTouches[0].clientY + ")";
-				break;
-		}
-	}
-}
-// EventUtil.addHandler(document, "touchstart", handleTouchEvent);
-// EventUtil.addHandler(document, "touchend", handleTouchEvent);
-// EventUtil.addHandler(document, "touchmove", handleTouchEvent);
