e0d09186524111af5169932de4043c0eb5d45631
ocean_deng <ocean_deng@163.com>
2016-07-10 22:50:52 +0800

    update

---
+++ app/scripts/javascript/event/clock.js
@@ -0,0 +1,34 @@
+/*
+* @Author: ocean_deng
+* @Date:   2016-07-10 22:29:24
+* @Last Modified by:   ocean_deng
+* @Last Modified time: 2016-07-10 22:50:04
+*/
+
+'use strict';
+whenReady(function(){
+	var clock = document.getElementById("clock");
+	var icon = new Image();
+	icon.src = "clock-icon.png";
+
+	// 没分钟显示一次时间
+	function displayTime(){
+		var now = new Date();
+		var hrs = now.getHours(),
+			mins = now.getMinutes();
+		if(mins < 10) mins = "0" + mins;
+		clock.innerHTML = hrs + ":" + mins;
+		setTimeout(displayTime, 60000);
+	}
+	displayTime();
+
+	clock.draggable = true;
+
+	clock.ondragstart = function(event){
+		var event = event || window.event;
+		var dt = event.dataTransfer;
+
+		dt.setData("Text", Date() + "\n");
+		if(dt.setDragImage) dt.setDragImage(icon, 0, 0);
+	};
+});
